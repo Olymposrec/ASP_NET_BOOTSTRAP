a6b8c15b8964b48f09f5a694229952877645e4fb
Melih <Olymposrec@users.noreply.github.com>
2020-07-15 01:58:10 +0300

    ASP.NET MVC için Bootstrap Örneği
    ASP.NET MVC için Bootstrap Örneği, örnek kodlar ve yararlı linklere ulaşabileceğiniz bir örnek.

---
+++ Bootstrap/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Bootstrap.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bootstrap.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Homepage()
+        {
+            return View();
+        }
+        public ActionResult Homepage2()
+        {
+            return View();
+        }
+        public ActionResult Homepage3()
+        {
+            List<Message> messages = new List<Message>();
+            messages.Add(new Message() { Level = 1, Text = "Uyarı Düzeyi 1" });
+            messages.Add(new Message() { Level = 2, Text = "Uyarı Düzeyi 2" });
+            messages.Add(new Message() { Level = 3, Text = "Uyarı Düzeyi 3" });
+            messages.Add(new Message() { Level = 4, Text = "Uyarı Düzeyi 4" });
+            return View(messages);
+        }
+    }
+}
